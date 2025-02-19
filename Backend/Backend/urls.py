82715db02ba6ea98c749a6f4bc3320f5ef4115ee
youssef <youss4923@gmail.com>
2025-02-19 20:44:47 +0200

    change url

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -5,7 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('', include('api.urls')),
     
     
      # for documing api
