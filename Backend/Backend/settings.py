cf82291fc6ad0953bb7df8511318e4c5869146a0
youssef <youss4923@gmail.com>
2025-02-19 20:36:52 +0200

    trial-railway1

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-g_w4o=#d6%9l34j&rjws4%o#15ebvu)df6@7t1&_*v&!a5$j#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-import os
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -52,6 +51,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
+import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -85,15 +85,11 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Backend.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',    
+    }
 }
 
 
