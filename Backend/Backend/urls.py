b344abc72b2b775b5f80bc0072046eb2fb34718a
youssef <youss4923@gmail.com>
2025-02-20 16:52:09 +0200

    update

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
      # for documing api
      path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     
 ]
